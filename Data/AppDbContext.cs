240990d179c1b0fe859d3f95d5dd2b9ac6604fc6
Domi <donniesan76@gmail.com>
2025-12-05 14:28:33 +0700

    Add DbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentCar.Web.Models.Entities;
+
+namespace RentCar.Web.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options)
+            : base(options) { }
+
+        public DbSet<LtPayment> LtPayment { get; set; }
+        public DbSet<MsCar> MsCar { get; set; }
+        public DbSet<MsCarImage> MsCarImage { get; set; } 
+        public DbSet<MsCustomer> MsCustomer { get; set; }
+        public DbSet<MsEmployee> MsEmployee { get; set; }
+        public DbSet<TrMaintenance> TrMaintenances { get; set; }
+        public DbSet<TrRental> TrRentals { get; set; }
+    }
+}
